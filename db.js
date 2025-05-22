7d1cf2c85b7a447e180807ce19ae2e13c1713e0c
anishshetty124 <anishshetty124@gmail.com>
2025-05-22 10:36:21 +0530

    final code for hosting

--- db.js
+++ db.js
@@ -7,8 +7,9 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD||'',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
 module.exports = pool;
